ee4647b270f54466cb34fbcd69491dd0bc99bf2b
RosaMPedro <85141361+RosaMPedro@users.noreply.github.com>
2021-11-11 14:34:26 -0600

    Creación del Menú(Interfaz) v2.4

---
+++ SistemaDeVenta/app/src/main/java/uv/moviles/sistemadeventa/HomeContentFragment.java
@@ -0,0 +1,39 @@
+package uv.moviles.sistemadeventa;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HomeContentFragment extends Fragment {
+
+  private static final String TEXT = "text";
+
+  public static HomeContentFragment newInstance(String text) {
+    HomeContentFragment frag = new HomeContentFragment();
+
+    Bundle args = new Bundle();
+    args.putString(TEXT, text);
+    frag.setArguments(args);
+
+    return frag;
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable
+          Bundle savedInstanceState) {
+    View layout = inflater.inflate(R.layout.home_fragment, container, false);
+
+    if (getArguments() != null) {
+      ((TextView) layout.findViewById(R.id.text)).setText(getArguments().getString(TEXT));
+    }
+
+    return layout;
+  }
+}
+
